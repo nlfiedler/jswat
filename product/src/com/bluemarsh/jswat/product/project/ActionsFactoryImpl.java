50a147a90669212fdd23fb4b124986b3950054d5
Nathan Fiedler <nathanfiedler@gmail.com>
2009-01-09 08:07:50 +0000

    Add missing @Override annotations.
    git-svn-id: file:///Users/nfiedler/jswat.svn/trunk@46 6539ee5b-5550-41f0-90e5-622d21111933

--- product/src/com/bluemarsh/jswat/product/project/ActionsFactoryImpl.java
+++ product/src/com/bluemarsh/jswat/product/project/ActionsFactoryImpl.java
@@ -36,66 +36,82 @@ import org.netbeans.spi.project.ui.support.ProjectActionPerformer;
  */
 public class ActionsFactoryImpl implements ActionsFactory {
 
+    @Override
     public Action setAsMainProjectAction() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public Action customizeProjectAction() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public Action openSubprojectsAction() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public Action closeProjectAction() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public Action newFileAction() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public Action deleteProjectAction() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public Action copyProjectAction() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public Action moveProjectAction() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public Action newProjectAction() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public Action projectCommandAction(String arg0, String arg1, Icon arg2) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public Action projectSensitiveAction(ProjectActionPerformer arg0, String arg1, Icon arg2) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public Action mainProjectCommandAction(String arg0, String arg1, Icon arg2) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public Action mainProjectSensitiveAction(ProjectActionPerformer arg0, String arg1, Icon arg2) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public Action fileCommandAction(String arg0, String arg1, Icon arg2) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public Action renameProjectAction() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public Action setProjectConfigurationAction() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
