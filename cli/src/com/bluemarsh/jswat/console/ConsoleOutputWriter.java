148c5be2e1faf4c04d07e1ae8066d0b7bccebe85
Nathan Fiedler <nlfiedler@yahoo.com>
2009-04-19 01:38:42 +0000

    Initial version of the console mode for JSwat.
    git-svn-id: file:///Users/nfiedler/jswat.svn/trunk@111 6539ee5b-5550-41f0-90e5-622d21111933

---
+++ cli/src/com/bluemarsh/jswat/console/ConsoleOutputWriter.java
@@ -0,0 +1,59 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is JSwat. The Initial Developer of the Original
+ * Software is Nathan L. Fiedler. Portions created by Nathan L. Fiedler
+ * are Copyright (C) 2009. All Rights Reserved.
+ *
+ * Contributor(s): Nathan L. Fiedler.
+ *
+ * $Id$
+ */
+package com.bluemarsh.jswat.console;
+
+import com.bluemarsh.jswat.core.output.OutputWriter;
+import java.io.PrintWriter;
+
+/**
+ * Implementation of OutputWriter for the console mode.
+ *
+ * @author  Nathan Fiedler
+ */
+public class ConsoleOutputWriter implements OutputWriter {
+
+    /** Sink for all output. */
+    private PrintWriter output;
+
+    /**
+     * Default constructor for ConsoleOutputWriter.
+     */
+    public ConsoleOutputWriter() {
+        output = System.console().writer();
+    }
+
+    @Override
+    public void ensureVisible() {
+        // Nothing to do.
+    }
+
+    @Override
+    public void printError(String msg) {
+        output.println(msg);
+    }
+
+    @Override
+    public void printOutput(String msg) {
+        output.println(msg);
+    }
+}
---
+++ cli/src/com/bluemarsh/jswat/console/Main.java
@@ -0,0 +1,169 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is JSwat. The Initial Developer of the Original
+ * Software is Nathan L. Fiedler. Portions created by Nathan L. Fiedler
+ * are Copyright (C) 2009. All Rights Reserved.
+ *
+ * Contributor(s): Nathan L. Fiedler.
+ *
+ * $Id$
+ */
+package com.bluemarsh.jswat.console;
+
+import com.bluemarsh.jswat.command.CommandException;
+import com.bluemarsh.jswat.command.CommandParser;
+import com.bluemarsh.jswat.command.CommandProvider;
+import com.bluemarsh.jswat.command.MissingArgumentsException;
+import com.bluemarsh.jswat.core.runtime.RuntimeManager;
+import com.bluemarsh.jswat.core.runtime.RuntimeProvider;
+import com.bluemarsh.jswat.core.session.Session;
+import com.bluemarsh.jswat.core.session.SessionManager;
+import com.bluemarsh.jswat.core.session.SessionProvider;
+import com.sun.jdi.Bootstrap;
+import java.io.Console;
+import java.io.PrintWriter;
+import org.openide.util.NbBundle;
+
+/**
+ * Bootstrap class for the console interface of JSwat.
+ *
+ * @author  Nathan Fiedler
+ */
+public class Main {
+
+    /**
+     * My class, private.
+     */
+    private Main() {
+        // None shall construct us.
+    }
+
+    /**
+     * Kicks off the application.
+     *
+     * @param  args  the command line arguments.
+     */
+    public static void main(String[] args) {
+        // Make sure we have a Console to work with.
+        // This appears to work from bash and emacs shell.
+        Console console = System.console();
+        if (console == null) {
+            // When run in NetBeans, or when the output streams are
+            // directed elsewhere, there will not be a Cosnole.
+            System.out.println(NbBundle.getMessage(Main.class, "MSG_Main_NoConsole"));
+            System.exit(1);
+        }
+
+        PrintWriter output = console.writer();
+
+        // Make sure we have the JPDA classes.
+        try {
+            Bootstrap.virtualMachineManager();
+        } catch (NoClassDefFoundError ncdfe) {
+            output.println(NbBundle.getMessage(Main.class, "MSG_Main_NoJPDA"));
+            System.exit(1);
+        }
+
+        // Add a shutdown hook to make sure we exit cleanly.
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // Save the runtimes to persistent storage.
+                RuntimeManager rm = RuntimeProvider.getRuntimeManager();
+                rm.saveRuntimes();
+                // Save the sessions to persistent storage and have them
+                // close down in preparation to exit.
+                SessionManager sm = SessionProvider.getSessionManager();
+                sm.saveSessions(true);
+                // Save the command aliases.
+                CommandParser parser = CommandProvider.getCommandParser();
+                parser.saveSettings();
+                System.out.println(NbBundle.getMessage(Main.class, "MSG_Main_Goodbye"));
+            }
+        }));
+
+        // Initialize the default session instance.
+        Session session = SessionProvider.getCurrentSession();
+
+        // Initialize the command parser and load the aliases.
+        CommandParser parser = CommandProvider.getCommandParser();
+        parser.loadSettings();
+        parser.setOutput(output);
+
+        // Display a helpful greeting.
+        output.println(NbBundle.getMessage(Main.class, "MSG_Main_Welcome"));
+
+        // If command line arguments were given, assume that they are
+        // commands to be run by the command parser.
+        if (args.length > 0) {
+            StringBuilder sb = new StringBuilder();
+            for (String arg : args) {
+                sb.append(arg);
+                sb.append(' ');
+            }
+            // Show what the user entered so the subsequent output makes sense.
+            String input = sb.toString().trim();
+            output.format("[%s] > %s\n", session.getProperty(
+                    Session.PROP_SESSION_NAME), input);
+            // Run the command the user provided.
+            performCommand(output, parser, input);
+        }
+
+        // Enter the main loop of processing user input.
+        while (true) {
+            String input = console.readLine("[%s] > ",
+                    session.getProperty(Session.PROP_SESSION_NAME));
+            input = input.trim();
+            if (input.equals("exit") || input.equals("quit")) {
+                // The shutdown hook will take care of cleaning up.
+                System.exit(0);
+            } else {
+                performCommand(output, parser, input);
+            }
+        }
+    }
+
+    /**
+     * Interprets the given command via the command parser.
+     *
+     * @param  output  where to write error messages.
+     * @param  parser  the command interpreter.
+     * @param  input   the input command.
+     */
+    private static void performCommand(PrintWriter output, CommandParser parser,
+            String input) {
+        // Send the input to the command parser, which will run the
+        // command and send output to the writer it was assigned earlier.
+        try {
+            parser.parseInput(input);
+        } catch (MissingArgumentsException mae) {
+            output.println(mae.getMessage());
+            output.println(NbBundle.getMessage(Main.class,
+                    "ERR_Main_HelpCommand"));
+        } catch (CommandException ce) {
+            // Print the message which should explain everything.
+            // If there is a root cause, show that, too.
+            output.println(ce.getMessage());
+            Throwable cause = ce.getCause();
+            if (cause != null) {
+                String cmsg = cause.getMessage();
+                if (cmsg != null) {
+                    output.println(cmsg);
+                }
+            }
+        }
+    }
+}
